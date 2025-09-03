7c71b206ee949ac6911b5419487e318c197e1650
SharpBlade <yepezedwin760@gmail.com>
2025-09-02 23:34:40 -0500

    fix-sin conf del next

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/AkkeBox",
-  assetPrefix: "/AkkeBox/",
   eslint: {
     ignoreDuringBuilds: true,
   },
