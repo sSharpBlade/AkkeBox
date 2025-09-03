820d7ebdc9498cea2f25e3f524b91a7eb852a67d
SharpBlade <yepezedwin760@gmail.com>
2025-09-02 23:16:28 -0500

    Github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/AkkeBox",
+  assetPrefix: "/AkkeBox/",
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +12,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
